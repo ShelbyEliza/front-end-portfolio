94272558c1a3e407ffbae02328c4e5f98f39cb3f
Shelby Shipley <shelbyelizashipley@gmail.com>
2022-10-19 08:07:39 -0700

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+// import speckleBg from "./assets/speckle-bg.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My Portfolio</h1>
-      </header>
+    <div
+      className="App"
+      // style={{
+      //   backgroundImage: speckleBg,
+      // }}
+    >
+      <BrowserRouter>
+        <header>
+          <h1>My Portfolio</h1>
+        </header>
+
+        <Routes>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/" element=<Home />></Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        He was a rather silent man, though perfectly good-humored. No clumsiness
+        of Arren's fretted him; he was companionable; there could be no better
+        shipmate, Arren thought. But he would go into his own thoughts and be
+        silent for hours on end, and then when he spoke there was a harshness in
+        his voice, and he would look right through Arren. This Did not weaken
+        the love the boy felt for him, but maybe it lessened liking somewhat; it
+        was a little awesome. Perhaps Sparrowhawk felt this, for in that foggy
+        night off the shores of Wathort he began to talk to Arren, rather
+        haltingly, about himself. "I do not want to go among men again
+        tomorrow," he said. "I've been pretending that I am free. . . . That
+        nothing's wrong in the world. That I'm not Archmage, not even sorcerer.
+        That I'm Hawk of Temere, without responsibilities or privileges, owing
+        nothing to anyone...." He stopped and after a while went on, "Try to
+        choose carefully, Arren, when the great choices must be made. When I was
+        young, I had to choose between the life of being and the life of doing.
+        And I leapt at the latter like a trout to a fly. But each deed you do,
+        each act, binds you to its consequences, and makes you act again and yet
+        again. Then very seldom do you come upon a space, a time like this,
+        between act and act, when you may stop and simply be. Or wonder who,
+        after all, you are."
+      </p>
+    </div>
+  );
+}
