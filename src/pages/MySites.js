fcaf0c8bdbeeb72407637a6259832bfcea1a3d1b
Shelby Shipley <shelbyelizashipley@gmail.com>
2022-12-16 14:48:06 -0800

    under the shelf link has been added to sites page

--- src/pages/MySites.js
+++ src/pages/MySites.js
@@ -1,9 +1,40 @@
 import styles from "../css/Sites.module.css";
 
+import { useCollection } from "../hooks/useCollection";
+
+import { useState, useEffect } from "react";
+
 export default function MySites() {
+  const [sites, setSites] = useState(null);
+  const [documentError, setDocumentError] = useState(null);
+  const { documents, error } = useCollection("sites");
+
+  useEffect(() => {
+    if (documents) {
+      setSites(documents);
+    }
+
+    if (error) {
+      setDocumentError(error);
+    }
+  }, [documents, error]);
+
   return (
-    <div className={styles.sites}>
+    <div className="site-content">
       <h1>My Sites</h1>
+      {sites && (
+        <section className={styles["sites-list"]}>
+          <ul>
+            {sites.map((site) => {
+              return (
+                <li key={site.title}>
+                  <a href={site.link}>{site.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+      )}
     </div>
   );
 }
